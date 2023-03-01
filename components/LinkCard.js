1e66e02d95872284d7f2ec476887fa693b3815d8
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-03-01 16:05:51 -0500

    Styling linkCard component

--- components/LinkCard.js
+++ components/LinkCard.js
@@ -8,16 +8,18 @@ const LinkCard = ({ href, title, image }) => {
       className='flex items-center w-full p-1 rounded-md hover:scale-105 transition-all border bg-gray-100 mb-3'
     >
       <div className='flex items-center w-full'>
-        {image && (
-          <Image
-            className='rounded-md'
-            src={image}
-            width={40}
-            height={40}
-            alt={title}
-          />
-        )}
-        <h2 className='font-semibold w-full text-center text-gray-700'>
+        <div className='w-10 h-10'>
+          {image && (
+            <Image
+              className='rounded-md'
+              src={image}
+              width={40}
+              height={40}
+              alt={title}
+            />
+          )}
+        </div>
+        <h2 className='flex justify-center font-semibold w-full text-gray-700 -ml-10'>
           {title}
         </h2>
       </div>
